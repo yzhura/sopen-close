921f463c93c64a46225afb18fffb840b0a61497b
yzhura <zhuravlev.yaroslav@gmail.com>
2022-10-14 20:51:57 +0300

    v1.0.2

--- dist/index.js
+++ dist/index.js
@@ -11,24 +11,24 @@ function ___$insertStyle(css) {
 
 ___$insertStyle(".open-close .opener {\n  cursor: pointer;\n}\n.open-close .opener.open .content {\n  font-size: 16px;\n}\n.open-close .opener.close .content {\n  font-size: 0;\n}\n.open-close .content {\n  transition: 0.2s ease-out all;\n}");
 
-var OPEN_CLASS = 'open';
-var CLOSE_CLASS = 'close';
-var OPENER_CLASS = 'opener';
 var SOpenClose = /** @class */ (function () {
     function SOpenClose(selector) {
+        this.OPEN_CLASS = 'OPEN';
+        this.CLOSE_CLASS = 'close';
+        this.OPENER_CLASS = 'opener';
         this.selector = selector;
         this.initOpenClose();
     }
     SOpenClose.prototype.initOpenClose = function () {
         var _this = this;
         var openClose = document.querySelector(this.selector);
-        var openers = Array.from(openClose.querySelectorAll(".".concat(OPENER_CLASS)));
-        openers.forEach(function (el) { return el.classList.add(CLOSE_CLASS); });
+        var openers = Array.from(openClose.querySelectorAll(".".concat(this.OPENER_CLASS)));
+        openers.forEach(function (el) { return el.classList.add(_this.CLOSE_CLASS); });
         openClose.addEventListener('click', function (event) {
             var target = event.target;
-            var opener = target.closest(".".concat(OPENER_CLASS));
+            var opener = target.closest(".".concat(_this.OPENER_CLASS));
             if (opener) {
-                if (opener.classList.contains(OPEN_CLASS)) {
+                if (opener.classList.contains(_this.OPEN_CLASS)) {
                     _this.closeAll(openers);
                     return;
                 }
@@ -38,12 +38,13 @@ var SOpenClose = /** @class */ (function () {
         });
     };
     SOpenClose.prototype.closeAll = function (openers) {
-        openers.forEach(function (el) { return el.classList.remove(OPEN_CLASS); });
-        openers.forEach(function (el) { return el.classList.add(CLOSE_CLASS); });
+        var _this = this;
+        openers.forEach(function (el) { return el.classList.remove(_this.OPEN_CLASS); });
+        openers.forEach(function (el) { return el.classList.add(_this.CLOSE_CLASS); });
     };
     SOpenClose.prototype.openCurrent = function (opener) {
-        opener.classList.add(OPEN_CLASS);
-        opener.classList.remove(CLOSE_CLASS);
+        opener.classList.add(this.OPEN_CLASS);
+        opener.classList.remove(this.CLOSE_CLASS);
     };
     return SOpenClose;
 }());
