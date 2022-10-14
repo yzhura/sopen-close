10c3b4056e8816a12ac01efddb9e3c799fb2358f
yzhura <zhuravlev.yaroslav@gmail.com>
2022-10-14 20:49:00 +0300

    moved classes into Class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import './style/index.scss';
 
-const OPEN_CLASS = 'open';
-const CLOSE_CLASS = 'close';
-const OPENER_CLASS = 'opener';
-
 class SOpenClose {
     selector: string;
+    private OPEN_CLASS: string = 'OPEN';
+    private CLOSE_CLASS: string = 'close';
+    private OPENER_CLASS: string = 'opener';
 
     constructor(selector: string) {
         this.selector = selector;
@@ -14,13 +13,13 @@ class SOpenClose {
 
     private initOpenClose() {
         const openClose = document.querySelector(this.selector) as HTMLElement;
-        const openers = Array.from(openClose.querySelectorAll(`.${OPENER_CLASS}`));
-        openers.forEach((el) => el.classList.add(CLOSE_CLASS));
+        const openers = Array.from(openClose.querySelectorAll(`.${this.OPENER_CLASS}`));
+        openers.forEach((el) => el.classList.add(this.CLOSE_CLASS));
         openClose.addEventListener('click', (event) => {
             const target = event.target as HTMLElement;
-            const opener = target.closest(`.${OPENER_CLASS}`);
+            const opener = target.closest(`.${this.OPENER_CLASS}`);
             if (opener) {
-                if(opener.classList.contains(OPEN_CLASS)) {
+                if(opener.classList.contains(this.OPEN_CLASS)) {
                     this.closeAll(openers);
                     return;
                 }
@@ -31,13 +30,13 @@ class SOpenClose {
     };
 
     private closeAll(openers: Element[]) {
-        openers.forEach((el) => el.classList.remove(OPEN_CLASS));
-        openers.forEach((el) => el.classList.add(CLOSE_CLASS));
+        openers.forEach((el) => el.classList.remove(this.OPEN_CLASS));
+        openers.forEach((el) => el.classList.add(this.CLOSE_CLASS));
     }
 
     private openCurrent(opener: Element) {
-        opener.classList.add(OPEN_CLASS);
-        opener.classList.remove(CLOSE_CLASS);
+        opener.classList.add(this.OPEN_CLASS);
+        opener.classList.remove(this.CLOSE_CLASS);
     }
 }
 
